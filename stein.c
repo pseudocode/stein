8a740b65c02b6551220ea77dbc500fd2a324d5fc
Michael Dingler <michael@pseudoco.de>
2021-01-07 23:47:23 +0100

    removed references to kilo

--- stein.c
+++ stein.c
@@ -32,7 +32,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define KILO_VERSION "0.0.1"
+#define STEIN_VERSION "0.0.1"
 
 #ifdef __linux__
 #define _POSIX_C_SOURCE 200809L
@@ -930,7 +930,7 @@ void editorRefreshScreen(void) {
             if (E.numrows == 0 && y == E.screenrows/3) {
                 char welcome[80];
                 int welcomelen = snprintf(welcome,sizeof(welcome),
-                                          "Kilo editor -- verison %s\x1b[0K\r\n", KILO_VERSION);
+                                          "Stein editor -- verison %s\x1b[0K\r\n", STEIN_VERSION);
                 int padding = (E.screencols-welcomelen)/2;
                 if (padding) {
                     abAppend(&ab,"~",1);
@@ -1049,10 +1049,10 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /* =============================== Find mode ================================ */
 
-#define KILO_QUERY_LEN 256
+#define STEIN_QUERY_LEN 256
 
 void editorFind(int fd) {
-    char query[KILO_QUERY_LEN+1] = {0};
+    char query[STEIN_QUERY_LEN+1] = {0};
     int qlen = 0;
     int last_match = -1; /* Last line where a match was found. -1 for none. */
     int find_next = 0; /* if 1 search next, if -1 search prev. */
@@ -1099,7 +1099,7 @@ void editorFind(int fd) {
             find_next = -1;
             }
         else if (isprint(c)) {
-            if (qlen < KILO_QUERY_LEN) {
+            if (qlen < STEIN_QUERY_LEN) {
                 query[qlen++] = c;
                 query[qlen] = '\0';
                 last_match = -1;
@@ -1237,11 +1237,11 @@ void editorMoveCursor(int key) {
 
 /* Process events arriving from the standard input, which is, the user
  * is typing stuff on the terminal. */
-#define KILO_QUIT_TIMES 3
+#define STEIN_QUIT_TIMES 3
 void editorProcessKeypress(int fd) {
     /* When the file is modified, requires Ctrl-q to be pressed N times
      * before actually quitting. */
-    static int quit_times = KILO_QUIT_TIMES;
+    static int quit_times = STEIN_QUIT_TIMES;
 
     int c = editorReadKey(fd);
     switch(c) {
@@ -1304,7 +1304,7 @@ void editorProcessKeypress(int fd) {
             break;
         }
 
-    quit_times = KILO_QUIT_TIMES; /* Reset it to the original value. */
+    quit_times = STEIN_QUIT_TIMES; /* Reset it to the original value. */
     }
 
 int editorFileWasModified(void) {
